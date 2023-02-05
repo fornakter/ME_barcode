2c966467f577c26d5e2332ba9e5650dc27fd7a73
AdamF <fornakter@gmail.com>
2023-02-05 14:52:15 +0100

    push #1

---
+++ main.py
@@ -0,0 +1,58 @@
+from kivy.properties import ObjectProperty
+from kivy.clock import mainthread
+from kivy.utils import platform
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+
+from camera4kivy import Preview
+from PIL import Image
+
+from pyzbar.pyzbar import decode
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class InfoScreen(MDScreen):
+    pass
+
+
+class ScanScreen(MDScreen):
+    def go_button(self):
+        print('jest')
+
+    def on_kv_post(self, obj):
+        self.ids.preview.connect_camera(enable_analyze_pixels=True, default_zoom=0.0)
+
+    @mainthread
+    def got_result(self, result):
+        self.ids.ti.text = str(result[0].decode("utf-8"))
+
+
+class ScanAnalyze(Preview):
+    extracted_data = ObjectProperty(None)
+
+    def analyze_pixels_callback(self, pixels, image_size, image_pos, scale, mirror):
+        pimage = Image.frombytes(mode='RGBA', size=image_size, data=pixels)
+        list_of_all_barcodes = decode(pimage)
+
+        if list_of_all_barcodes:
+            if self.extracted_data:
+                self.extracted_data(list_of_all_barcodes[0])
+            else:
+                print("Not found")
+
+
+class Me_App(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.CAMERA])  # , Permission.RECORD_AUDIO
+        return ScanScreen()
+
+
+if __name__ == '__main__':
+    Me_App().run()
